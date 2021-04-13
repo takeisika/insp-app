884201d4cd20856e71fe08f568d05f9f13ff4cae
takehiro <takeishi19990912@gmail.com>
2021-04-14 05:47:05 +0900

    e

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_202158) do
+ActiveRecord::Schema.define(version: 2021_04_12_144621) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,16 +85,6 @@ ActiveRecord::Schema.define(version: 2021_04_13_202158) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image_name"
-    t.string "email"
-    t.text "introduction"
   end
 
 end
